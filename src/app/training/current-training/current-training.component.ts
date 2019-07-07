ef3925c1511816ca742c25e8c9e2c9e7a9b15f2a
Ivan Grushevich <githubmail911@gmail.com>
2019-07-08 02:17:26 +0300

    Add spinner to current training page

--- src/app/training/current-training/current-training.component.ts
+++ src/app/training/current-training/current-training.component.ts
@@ -6,10 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./current-training.component.scss']
 })
 export class CurrentTrainingComponent implements OnInit {
+  progress: number;
+  interval: any;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    this.progress = 0;
+    this.interval = setInterval(() => {
+      this.progress += 5;
+
+      if (this.progress >= 100) {
+        clearInterval(this.interval);
+      }
+    }, 200);
   }
 
+  onStop() {
+    clearInterval(this.interval);
+  }
 }
