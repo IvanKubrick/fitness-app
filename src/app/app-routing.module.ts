dc1eb41da4ab6b754e4871ccffce9331f50ead95
Ivan Grushevich <githubmail911@gmail.com>
2019-07-10 22:22:31 +0300

    Add auth guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,12 +7,13 @@ import { SignupComponent } from './auth/signup/signup.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { LoginComponent } from './auth/login/login.component';
 import { TrainingComponent } from './training/training.component';
+import { AuthGuard } from './auth/auth.guad';
 
 const routes: Routes = [
   { path: '', component: WelcomeComponent },
   { path: 'signup', component: SignupComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'training', component: TrainingComponent }
+  { path: 'training', component: TrainingComponent, canActivate: [AuthGuard] }
 ];
 
 @NgModule({
@@ -21,6 +22,7 @@ const routes: Routes = [
     BrowserAnimationsModule,
     MaterialModule
   ],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [AuthGuard]
 })
 export class AppRoutingModule {}
